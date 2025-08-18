ab7d1d379897aca09d84b14f8808c6f7e7ea797d
SAY_bin <96761413+SAY683@users.noreply.github.com>
2025-08-18 17:11:51 +0800

    修改优化

---
+++ Base/JS/pdf-data.js
@@ -0,0 +1,28 @@
+// js/pdf-data.js
+
+const pdfFiles = [
+    {
+        title: "太易神式(0.9)",
+        file: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/Miscellanies/%E5%A4%AA%E6%98%93%E7%A5%9E%E5%BC%8F(0.9).pdf"
+    },
+    {
+        title: "源始",
+        file: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/Miscellanies/%E6%BA%90%E5%A7%8B.pdf"
+    },
+    {
+        title: "元皇经",
+        file: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/Miscellanies/%E5%85%83%E7%9A%87%E7%BB%8F.pdf"
+    },
+    {
+        title: "太易 - 太伊原枢（纪录）",
+        file: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/Miscellanies/%E5%A4%AA%E6%98%93%20-%20%E5%A4%AA%E4%BC%8A%E5%8E%9F%E6%9E%A2%EF%BC%88%E7%BA%AA%E5%BD%95%EF%BC%89.pdf"
+    },
+    {
+        title: "人偶之匣（魔女的盒子）",
+        file: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/Miscellanies/%E4%BA%BA%E5%81%B6%E4%B9%8B%E5%8C%A3%EF%BC%88%E9%AD%94%E5%A5%B3%E7%9A%84%E7%9B%92%E5%AD%90%EF%BC%89.pdf"
+    }
+    {
+        title: "P0P1-VTS模型",
+        file: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/Miscellanies/P0P1-VTS%E6%A8%A1%E5%9E%8B.pdf"
+    },
+];
