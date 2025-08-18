42feae0255891354b548b0a8bc9da07436afea0f
SAY_bin <96761413+SAY683@users.noreply.github.com>
2025-08-18 17:34:33 +0800

    减少不必要的版本

--- Base/JS/pdf-data.js
+++ Base/JS/pdf-data.js
@@ -32,30 +32,30 @@ const pdfFiles = [
         remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/Miscellanies/P0P1-VTS%E6%A8%A1%E5%9E%8B.pdf",
         localFile: "Miscellanies/P0P1-VTS模型.pdf"
     },
-    // --- 您新增的文件 ---
-    {
-        title: "琉璃庭院 - Alice",
-        remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E7%90%89%E7%92%83%E5%BA%AD%E9%99%A2%20-%20Alice.pdf",
-        localFile: "Miscellanies/琉璃庭院 - Alice.pdf"
-    },
-    {
-        title: "梦境漫游纪",
-        remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E6%A2%A6%E5%A2%83%E6%BC%AB%E6%B8%B8%E7%BA%AA.pdf",
-        localFile: "Miscellanies/梦境漫游纪.pdf"
-    },
-    {
-        title: "摩诃室利（奥源）",
-        remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E6%91%A9%E8%AF%83%E5%AE%A4%E5%88%A9%EF%BC%88%E5%A5%A5%E6%BA%90%EF%BC%89.pdf",
-        localFile: "Miscellanies/摩诃室利（奥源）.pdf"
-    },
-    {
-        title: "太上洞庭自然经",
-        remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E5%A4%AA%E4%B8%8A%E6%B4%9E%E5%BA%AD%E8%87%AA%E7%84%B6%E7%BB%8F.pdf",
-        localFile: "Miscellanies/太上洞庭自然经.pdf"
-    },
-    {
-        title: "天海之泪（世界）",
-        remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E5%A4%A9%E6%B5%B7%E4%B9%8B%E6%B3%AA%EF%BC%88%E4%B8%96%E7%95%8C%EF%BC%89.pdf",
-        localFile: "Miscellanies/天海之泪（世界）.pdf"
-    }
+    // // --- 您新增的文件 ---
+    // {
+    //     title: "琉璃庭院 - Alice",
+    //     remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E7%90%89%E7%92%83%E5%BA%AD%E9%99%A2%20-%20Alice.pdf",
+    //     localFile: "Miscellanies/琉璃庭院 - Alice.pdf"
+    // },
+    // {
+    //     title: "梦境漫游纪",
+    //     remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E6%A2%A6%E5%A2%83%E6%BC%AB%E6%B8%B8%E7%BA%AA.pdf",
+    //     localFile: "Miscellanies/梦境漫游纪.pdf"
+    // },
+    // {
+    //     title: "摩诃室利（奥源）",
+    //     remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E6%91%A9%E8%AF%83%E5%AE%A4%E5%88%A9%EF%BC%88%E5%A5%A5%E6%BA%90%EF%BC%89.pdf",
+    //     localFile: "Miscellanies/摩诃室利（奥源）.pdf"
+    // },
+    // {
+    //     title: "太上洞庭自然经",
+    //     remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E5%A4%AA%E4%B8%8A%E6%B4%9E%E5%BA%AD%E8%87%AA%E7%84%B6%E7%BB%8F.pdf",
+    //     localFile: "Miscellanies/太上洞庭自然经.pdf"
+    // },
+    // {
+    //     title: "天海之泪（世界）",
+    //     remoteFile: "https://raw.githubusercontent.com/SAY683/Annals-of-the-Great-Simplicity/main/%E5%A4%A9%E6%B5%B7%E4%B9%8B%E6%B3%AA%EF%BC%88%E4%B8%96%E7%95%8C%EF%BC%89.pdf",
+    //     localFile: "Miscellanies/天海之泪（世界）.pdf"
+    // }
 ];
